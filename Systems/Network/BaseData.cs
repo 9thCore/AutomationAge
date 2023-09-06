f60f6d9d75bb39aca90f7a11ad68f140be528ad4
9thCore <38700349+9thCore@users.noreply.github.com>
2023-09-06 09:19:39 +0300

    update BaseData
    Now it just has a list of NetworkContainers which do all the work of adding, removing items etc

--- Systems/Network/BaseData.cs
+++ Systems/Network/BaseData.cs
@@ -5,24 +5,6 @@ namespace AutomationAge.Systems.Network
 {
     internal class BaseData : MonoBehaviour
     {
-        List<StorageContainer> storageContainers = new List<StorageContainer>();
-
-        public void BroadcastGameObject(GameObject obj)
-        {
-            if (obj.TryGetComponent(out StorageContainer sContainer))
-            {
-                storageContainers.Add(sContainer);
-                return;
-            }
-        }
-
-        public void StopBroadcastingGameObject(GameObject obj)
-        {
-            if (obj.TryGetComponent(out StorageContainer sContainer))
-            {
-                storageContainers.Remove(sContainer);
-                return;
-            }
-        }
+        public List<NetworkContainer> networkContainers = new List<NetworkContainer>();
     }
 }
