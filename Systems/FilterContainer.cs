d4e7046167fc02881ebcd57ebf200a7982f16f7b
9thCore <38700349+9thCore@users.noreply.github.com>
2023-09-07 17:50:18 +0300

    Class FilterContainer
    Handles filter-related tasks like making its container only accept unique items or returning a list of all items

---
+++ Systems/FilterContainer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UWE;
+using static CraftNode;
+
+namespace AutomationAge.Systems
+{
+    internal class FilterContainer
+    {
+        public ItemsContainer container { get; private set; }
+
+        public void QueueAttach(GameObject gameObject)
+        {
+            if (!gameObject.TryGetComponent(out StorageContainer storageContainer))
+            {
+                throw new InvalidOperationException("missing StorageContainer component");
+            }
+
+            CoroutineHost.StartCoroutine(Attach(storageContainer));
+        }
+
+        public List<InventoryItem> GetItems()
+        {
+            List<InventoryItem> items = new List<InventoryItem>();
+            foreach (KeyValuePair<TechType, ItemsContainer.ItemGroup> pair in container._items)
+            {
+                foreach(InventoryItem item in pair.Value.items)
+                {
+                    items.Add(item);
+                }
+            }
+            return items;
+        }
+
+        private IEnumerator Attach(StorageContainer storageContainer)
+        {
+            yield return new WaitUntil(() => storageContainer.container != null);
+
+            container = storageContainer.container;
+            container.isAllowedToAdd += (Pickupable pickupable, bool verbose) =>
+            {
+                return !container.Contains(pickupable.GetTechType());
+            };
+        }
+    }
+}
