b20673f871c339a30c2787cbc1e8d0be7ddfee77
9thCore <38700349+9thCore@users.noreply.github.com>
2023-09-25 16:08:09 +0300

    no longer cache assets because that's dumb lol

--- Systems/Assets.cs
+++ Systems/Assets.cs
@@ -10,32 +10,19 @@ namespace AutomationAge.Systems
         public static string AssetsPath = Path.Combine(Plugin.ModPath, "Assets");
         public static AssetBundle MainBundle { get; private set; }
 
-        public static Dictionary<string, Texture2D> Sprites { get; private set; } = new Dictionary<string, Texture2D>();
-        public static Dictionary<string, GameObject> GameObjects { get; private set; } = new Dictionary<string, GameObject>();
-
         public static void Prepare()
         {
             MainBundle = AssetBundle.LoadFromFile(Path.Combine(AssetsPath, "automationage"));
-
-            foreach (Texture2D tex in MainBundle.LoadAllAssets<Texture2D>())
-            {
-                Sprites[tex.name] = tex;
-            }
-
-            foreach (GameObject go in MainBundle.LoadAllAssets<GameObject>())
-            {
-                GameObjects[go.name] = go;
-            }
         }
 
         public static Texture2D GetTexture2D(string name)
         {
-            return Sprites[name];
+            return MainBundle.LoadAsset<Texture2D>(name);
         }
 
         public static GameObject GetGameObject(string name)
         {
-            return GameObjects[name];
+            return MainBundle.LoadAsset<GameObject>(name);
         }
 
         public static Sprite GetSprite(string name)
